0edb5deea47167a4d30af9214da13787591d7eb4
Lena Ringstad Olsen <lenaringstado@gmail.com>
2020-04-07 08:10:07 +0200

    endret filtrering på liggetid til 40

--- R/NIRtabellerBeredskap.R
+++ R/NIRtabellerBeredskap.R
@@ -140,9 +140,9 @@ oppsumFerdigeRegTab <- function(RegData, valgtRHF='Alle', bekr=9, erMann=9, resp
   AntBruktResp <- sum(RegData$MechanicalRespirator==1, na.rm=T)
   AntBruktECMO <- sum(RegData$ECMOTid>0, na.rm=T)
   #AntUtInt <- sum(RegData$DateDischargedIntensive>0, na.rm=T)
-  Liggetid <- summary(RegData$liggetid[RegData$liggetid < 30], na.rm = T)
-  RespTid <- summary(RegData$RespTid[RegData$RespTid < 30], na.rm = T)
-  ECMOtid <- summary(RegData$ECMOTid[RegData$ECMOTid < 30], na.rm = T)
+  Liggetid <- summary(RegData$liggetid[RegData$liggetid < 40], na.rm = T)
+  RespTid <- summary(RegData$RespTid[RegData$RespTid < 40], na.rm = T)
+  ECMOtid <- summary(RegData$ECMOTid[RegData$ECMOTid < 40], na.rm = T)
   Alder <- summary(RegData$Alder, na.rm = T)
   AntDod <- sum(RegData$DischargedIntensivStatus==1, na.rm=T)
 
