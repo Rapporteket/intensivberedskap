1bc5c0a13bb98b4aca7f51c3ad831094202e101e
Lena Ringstad Olsen <lenaringstado@gmail.com>
2021-02-10 18:57:33 +0100

    oppdat  eivh

--- inst/shinyApps/intensivberedskap/app.R
+++ inst/shinyApps/intensivberedskap/app.R
@@ -501,11 +501,11 @@ server <- function(input, output, session) {
       shinyjs::hide(id = 'CoroRappTxt')
       hideTab(inputId = "hovedark", target = "Abonnement")
     }
-    if (!(brukernavn %in% c('lenaro', 'Reidar', 'eabu', 'EIVH', 'mariawa-he'))) {  #(brukernavn == 'jlaake') {
+    if (!(brukernavn %in% c('lenaro', 'Reidar', 'eabu', 'eivh', 'mariawa-he'))) {  #(brukernavn == 'jlaake') {
       shinyjs::hide(id = 'lastNed_dataBeredNIRraa')
       shinyjs::hide(id = 'lastNed_dataBeredNIR')
     }
-    if (!(brukernavn %in% c('lenaro', 'Reidar', 'eabu', 'EIVH', 'jlaake', 'mariawa-he'))) { #jlaake-ikke datafiler
+    if (!(brukernavn %in% c('lenaro', 'Reidar', 'eabu', 'eivh', 'jlaake', 'mariawa-he'))) { #jlaake-ikke datafiler
       hideTab(inputId = "hovedark", target = "Registeradmin.")
       #hideTab(inputId = "hovedark", target = "Fordelingsfigurer")
     }
